46c36772eb4fefdd5ed0f45880ec057ba885a452
rubencunyas <ruben.cunba@gmail.com>
2023-04-12 14:43:46 -0500

    Usando bootstrap5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting={'Bienvenidos'} />
+      <ItemListContainer greeting={'Realiza tu pedido'} />
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,39 @@
 import CartWidget from "../CartWidget/CartWidget"
 
-const NavBar = () =>{
-    return(
+const NavBar = () => {
+    return (
         <nav>
-            <h3>Pedidos de restaurante</h3>
-            <div>
-                <button>Entradas</button>
-                <button>Platos de fondo</button>
-                <button>Bebidas frías</button>
-            </div>
-            <CartWidget />
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container">
+                    <div class="col">
+                        <a class="navbar-brand" href="#">Pedidos GO!</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div class="col collapse navbar-collapse" id="navbarContent">
+                        <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="#">Entradas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Platos de fondo</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Bebidas frías</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <CartWidget />
+                    </div>
+
+                </div>
+            </nav>
+            
         </nav>
+
+
     )
 }
 
